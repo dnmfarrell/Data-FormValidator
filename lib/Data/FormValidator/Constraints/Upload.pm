5d485fad701f6adb7313b5b288c80eed477c24e8
mark <mark@summersault.com>
2005-03-25 20:41:23 +0000

    refactor to use a simpler closure-based interface

--- lib/Data/FormValidator/Constraints/Upload.pm
+++ lib/Data/FormValidator/Constraints/Upload.pm
@@ -3,7 +3,7 @@ package Data::FormValidator::Constraints::Upload;
 use strict;
 
 require Exporter;
-use vars qw($VERSION @ISA @EXPORT);
+use vars qw($VERSION @ISA @EXPORT @EXPORT_OK);
 @ISA = qw(Exporter);
 
 # Items to export into callers namespace by default. Note: do not export
@@ -20,19 +20,52 @@ use vars qw($VERSION @ISA @EXPORT);
 	valid_file_max_bytes	
 );
 
-$VERSION = '0.71';
+@EXPORT_OK = qw(
+	file_format
+	image_max_dimensions
+	file_max_bytes
+);
+
+$VERSION = 1.00_01;
+
+sub file_format {
+	my %params = @_;
+	return sub {
+		my $self = shift; 
+		$self->set_current_constraint_name('file_format');
+		valid_file_format($self,\%params);
+	}
+}
+
+sub image_max_dimensions {
+	my ($w,$h) = @_;
+	return sub {
+		my $self = shift;
+		$self->set_current_constraint_name('image_max_dimensions');
+		valid_image_max_dimensions(\$w,\$h);
+	}
+}
+
+sub file_max_bytes {
+	my ($max_bytes) = @_;
+	return sub {
+		my $self = shift;
+		$self->set_current_constraint_name('file_max_bytes');
+		valid_image_max_dimensions(\$max_bytes);
+	}
+}
 
 sub valid_file_format {
 	my $self = shift;
 	$self->isa('Data::FormValidator::Results') ||
-		die "valid_file_format: first argument is not a Data::FormValidator::Results object.
+		die "file_format: first argument is not a Data::FormValidator::Results object.
 			Check that you used 'constraint_method' and not 'constraint'";
 	my $params = shift || {};
-	if (ref $params ne 'HASH' ) {
-		die "valid_file_format: hash reference expected. Make sure you have
-		included 'params => []' in your constraint definition, even if there
-		are no additional arguments";
-	}
+	# if (ref $params ne 'HASH' ) {
+	# 	die "format: hash reference expected. Make sure you have
+	# 	included 'params => []' in your constraint definition, even if there
+	# 	are no additional arguments";
+	# }
 
 	my $q = $self->get_input_data;
 
@@ -196,25 +229,20 @@ Data::FormValidator::Constraints::Upload - Validate File Uploads
     # Be sure to use a CGI.pm object as the form input
     # when using this constraint
     my $q = new CGI;
+
+	use Data::FormValidator::Constraints::Upload qw(
+			file_format
+			file_max_bytes
+		    image_max_dimensions
+	);
     my $dfv = Data::FormValidator->check($q,$my_profile);
 
 	# In a Data::FormValidator Profile:
-	validator_packages => [qw(Data::FormValidator::Constraints::Upload)],
 	constraints => {
 		image_name => [
-			{
-				constraint_method => 'file_format',
-				params => [],
-			},
-			{
-				constraint_method => 'file_max_bytes',
-				params => [\100],
-			},
-			{
-				constraint_method => 'image_max_dimensions',
-				params => [\200,\200],
-			},
-
+			file_format(),
+			file_max_bytes(10),
+		    image_max_dimensions(200,200),	
 		 ],
 	}
 
@@ -236,16 +264,13 @@ be a CGI.pm object.
 =item file_format
 
 This function checks the format of the file, based on the MIME type if it's
-available, and a case-insensitive version of the file extension otherwise. By default, 
-it tries to validate JPEG, GIF and PNG images. The params are:
+available, and a case-insensitive version of the file extension otherwise. By
+default, it tries to validate JPEG, GIF and PNG images. The params are:
 
  optional hash reference of parameters. A key named I<mime_types> points to
  array refererences of valid values.
 
-	constraint_method => 'file_format',
-	params => [{
-		mime_types => [qw!image/jpeg image/gif image/png!],
-	}],
+   file_format( mime_types => [qw!image/jpeg image/gif image/png!] );
 
 Calling this function sets some meta data which can be retrieved through
 the C<meta()> method of the Data::FormValidator::Results object.
@@ -257,7 +282,6 @@ we'll use a MIME type from the browser if one has been provided. Otherwise, we
 give up. The extension we return is based on the MIME type we found, rather
 than trusting the one that was uploaded.
 
-
 =item file_max_bytes
 
 This function checks the maximum size of an uploaded file. By default,
@@ -265,8 +289,7 @@ it checks to make sure files are smaller than 1 Meg. The params are:
 
  reference to max file size in bytes
 
-	constraint_method => 'file_max_bytes',
-	params => [\1024], # 1 k
+ 	file_max_bytes(1024), # 1 k
 
 Calling this function sets some meta data which can be retrieved through
 the C<meta()> method of the Data::FormValidator::Results object.
@@ -280,14 +303,33 @@ some maximum dimensions. The params are:
  reference to max pixel width
  reference to max pixel height
 
-	constraint_method => 'image_max_dimensions',
-	params => [\200,\200],
+    image_max_dimensions(200,200),
 
 Calling this function sets some meta data which can be retrieved through
 the C<meta()> method of the Data::FormValidator::Results object.
 The meta data added is C<width> and C<height>.
 
 =back
+
+=head2 BACKWARDS COMPATIBILITY
+
+An older more awkward interface to the constraints in this module is still supported.
+To use it, you have to load the package with 'validator_packages', and call each
+constraint in a hashref style, passing the the parameters by reference. It looks
+like this:
+
+	validator_packages => [qw(Data::FormValidator::Constraints::Upload)],
+	constraints => {
+		image_name => [
+			{
+		    	constraint_method => 'image_max_dimensions', 
+				params => [\200,\200],
+			}
+		 ],
+	}
+
+I told you it was more awkward. That was before I grokked the magic of closures, which 
+is what drives the current interface.
 
 =head1 SEE ALSO
 
@@ -299,7 +341,7 @@ Mark Stosberg, E<lt>mark@summersault.comE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2003 by Mark Stosberg
+Copyright 2003-2005 by Mark Stosberg
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself. 
