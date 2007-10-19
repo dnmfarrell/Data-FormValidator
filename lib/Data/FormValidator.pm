342ed6619a5b216ba0a10aeae2ebec0bb133bee9
Mark Stosberg <mark@summersault.com>
2007-10-19 19:39:37 +0000

    version bump for 4.52.

--- lib/Data/FormValidator.pm
+++ lib/Data/FormValidator.pm
@@ -33,7 +33,7 @@ use Data::FormValidator::Constraints qw(:validators :matchers);
 
 use vars qw( $VERSION $AUTOLOAD @ISA @EXPORT_OK %EXPORT_TAGS );
 
-$VERSION = '4.51';
+$VERSION = '4.52';
 
 require Exporter;
 @ISA = qw(Exporter);
