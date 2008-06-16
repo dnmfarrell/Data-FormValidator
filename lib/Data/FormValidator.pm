92e3e45159014be7c45e98d5a6a0508033f501c9
Mark Stosberg <mark@summersault.com>
2008-06-16 18:40:16 +0000

    manifest update and version bumps

--- lib/Data/FormValidator.pm
+++ lib/Data/FormValidator.pm
@@ -33,7 +33,7 @@ use Data::FormValidator::Constraints qw(:validators :matchers);
 
 use vars qw( $VERSION $AUTOLOAD @ISA @EXPORT_OK %EXPORT_TAGS );
 
-$VERSION = '4.60';
+$VERSION = '4.61';
 
 require Exporter;
 @ISA = qw(Exporter);
