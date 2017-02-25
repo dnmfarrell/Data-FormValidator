b68ba462485ea6850ad605a131a4089f7ec207bd
David Farrell <davidnmfarrell@gmail.com>
2017-02-25 10:09:28 -0500

    version bump

--- lib/Data/FormValidator.pm
+++ lib/Data/FormValidator.pm
@@ -31,7 +31,7 @@ use Data::FormValidator::Results;
 use Data::FormValidator::Filters ':filters';
 use Data::FormValidator::Constraints qw(:validators :matchers);
 
-our $VERSION = '4.83';
+our $VERSION = '4.84';
 
 our %EXPORT_TAGS = (
     filters => [qw/
